8c8784f537ced8e1f6d8cc3c265c8a96ff1c3eb8
Cody Swendrowski <cody@ironmoosedevelopment.com>
2020-10-31 00:02:11 -0500

    Add override option

--- scripts/module.js
+++ scripts/module.js
@@ -1,9 +1,20 @@
 (() => { })();
 
 Hooks.once('init', async function() {
-
+    game.settings.register('remote-address-override', 'override', {
+        name: game.i18n.localize("RAO.SETTINGS.OverrideName"),
+        hint: game.i18n.localize("RAO.SETTINGS.OverrideHint"),
+        scope: 'world',
+        config: true,
+        default: '',
+        type: String,
+      });
 });
 
 Hooks.once('ready', async function() {
-
+    let override = game.settings.get('remote-address-override', 'override');
+    if (override && override != "") {
+        console.log("Overriding remove address per World Setting");
+        game.data.addresses.remote = override;
+    }
 });
